3498038a163300d3cb60c06ae4b16d344d0169d4
Praveen Kothapally <praveenkothapally18@gmail.com>
2023-08-21 17:50:55 +0530

    added if condition

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-X1=$1
+X=$1
 
-if[$X1 -gt 10 ]
+if[$X -gt 10 ]
 then
-    echo "$X1 is greater than 10"
+    echo "$X is greater than 10"
 else
-    echo "$X1 is not greater than 10"
+    echo "$X is not greater than 10"
 fi 
