024b8b3fe3f6b5351beab1fbeaa73d315999f0b5
Praveen Kothapally <praveenkothapally18@gmail.com>
2023-08-21 13:58:15 +0530

    adding new lines

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Good Morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I am good, How are you $PERSON1"
