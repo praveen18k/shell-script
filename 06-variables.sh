8cf6c22e73b3c00b977d55ad98541f5023470e7a
Praveen Kothapally <praveenkothapally18@gmail.com>
2023-08-21 15:23:24 +0530

    adding new lines

--- 06-variables.sh
+++ 06-variables.sh
@@ -4,11 +4,13 @@ echo "Please enter your username"
 
 read -s USERNAME # this is the variable where the values of the one user entered
 
+#the below line is only for validation in real time we wont see Username
 echo "Username entered is:$USERNAME"
 
 echo "Please enter password:"
 
 read -s PASSWORD 
 
+#the below line is only for validation in real time we wont see Password
 echo "Password entered is:$PASSWORD"
 
