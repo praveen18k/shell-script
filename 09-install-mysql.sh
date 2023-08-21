af3717a6f5d953a4232192a39616e274701c9420
Praveen Kothapally <praveenkothapally18@gmail.com>
2023-08-21 20:15:16 +0530

    mysql installation throush script

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -8,5 +8,13 @@ then
     echo " ERROR: Please Run this script with root user"
     exit 1
 fi
+#it is our responsibility again to check installation is success or not
+yum install mysqllll -y
 
-yum install mysql -y
+if [ $ -ne 0 ]
+then 
+    echo "Installation of mysql is error"
+    exit 1
+else   
+    echo "Installation og mysql is success"
+fi
