9c9f5474323ef4d307ee7bc0807bdeaefc9f049b
Praveen Kothapally <praveenkothapally18@gmail.com>
2023-08-21 15:17:28 +0530

    adding new lines

---
+++ 06-variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Please enter your username"
+
+read USERNAME # this is the variable where the values of the one user entered
+
+echo "Username entered is:$USERNAME"
+
+echo "Please enter password:"
+
+read PASSWORD 
+
+echo "Password entered is:$PASSWORD"
+
