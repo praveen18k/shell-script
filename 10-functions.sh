7c16eaafdae42a5126b7754063afe0ed4eb520f0
Praveen Kothapally <praveenkothapally18@gmail.com>
2023-08-22 00:07:00 +0530

    mysql installation throush script

--- 10-functions.sh
+++ 10-functions.sh
@@ -2,7 +2,10 @@
 
 #our program goal is to install mysql
 
-USERID=$(id -u)
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
 #Function name is VALIDATE & this validate the previous command and inform user it is success or failure
 VALIDATE(){
     #$1 --> it will receive the argument 1
@@ -14,16 +17,19 @@ VALIDATE(){
         echo "$2... SUCCESS"
     fi  
 }
+
+USERID=$(id -u)
+
 if [ $USERID -ne 0 ]
 then
     echo " ERROR: Please Run this script with root user"
     exit 1
 fi
 #it is our responsibility again to check installation is success or not
-yum install mysql -y
+yum install mysql -y &>>$LOGFILE
 
 VALIDATE $? "Installing MySQL"
 
-yum install postfix -y
+yum install postfix -y &>>$LOGFILE
 
 VALIDATE $? "Installing postfix"
