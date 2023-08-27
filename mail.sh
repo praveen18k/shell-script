5f4091918a75f0103a62e766e85416fb12b38eb4
Praveen Kothapally <praveenkothapally18@gmail.com>
2023-08-28 02:38:10 +0530

    Updated script

--- mail.sh
+++ mail.sh
@@ -5,7 +5,8 @@
 
 To_ADDRESS=$1
 SUBJECT=$2
-BODY=$3
+BODY=$(sed -e 's/[]\/$".^[]/\\&/g' <<< $3)
+echo "escaped content: $BODY"
 TEAM_NAME=$4
 ALERT_TYPE=$5
 
@@ -13,6 +14,6 @@ ALERT_TYPE=$5
 
 #to write or Edit "BODY" use 'sed' editor (streamline editor)
 
-FINAL_BODY=$(sed -e 's/TEAM_NAME/DevOps Team/g' -e 's/ALERT_TYPE/High Disck Usage/g' -es/MESSAGE/$BODY" template.html)
+FINAL_BODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY/g" template.html)
 
-echo "$FINAL_BODY" | mail -s "$SUBJECT" $TO_ADDRESS
+echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")"  "$TO_ADDRESS"
